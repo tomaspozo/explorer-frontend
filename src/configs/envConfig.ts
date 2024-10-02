a5cbcba5ea38073faabb1be1b5e30b3f0ea54054
Tomas Pozo <tomaspozogarzon@gmail.com>
2024-10-01 22:31:05 -0500

    feat: Add qubic metrics service

--- src/configs/envConfig.ts
+++ src/configs/envConfig.ts
@@ -1,11 +1,13 @@
 type EnvConfig = {
   QLI_API_URL: string
   ARCHIVER_API_URL: string
+  METRICS_API_URL: string
 }
 
 export const envConfig: EnvConfig = {
   QLI_API_URL: import.meta.env.VITE_QLI_API_URL,
-  ARCHIVER_API_URL: import.meta.env.VITE_ARCHIVER_API_URL
+  ARCHIVER_API_URL: import.meta.env.VITE_ARCHIVER_API_URL,
+  METRICS_API_URL: import.meta.env.VITE_QUBIC_METRICS_API_URL
 }
 
 export default envConfig
