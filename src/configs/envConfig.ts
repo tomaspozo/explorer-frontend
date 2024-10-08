6a80f366d297a05828a481991469ef4eb3ff12ad
Tomas Pozo <tomaspozogarzon@gmail.com>
2024-10-08 13:04:52 -0500

    Add metrics api token

--- src/configs/envConfig.ts
+++ src/configs/envConfig.ts
@@ -2,12 +2,14 @@ type EnvConfig = {
   QLI_API_URL: string
   ARCHIVER_API_URL: string
   METRICS_API_URL: string
+  METRICS_API_TOKEN: string
 }
 
 export const envConfig: EnvConfig = {
   QLI_API_URL: import.meta.env.VITE_QLI_API_URL,
   ARCHIVER_API_URL: import.meta.env.VITE_ARCHIVER_API_URL,
-  METRICS_API_URL: import.meta.env.VITE_METRICS_API_URL
+  METRICS_API_URL: import.meta.env.VITE_METRICS_API_URL,
+  METRICS_API_TOKEN: import.meta.env.VITE_METRICS_API_TOKEN
 }
 
 export default envConfig
